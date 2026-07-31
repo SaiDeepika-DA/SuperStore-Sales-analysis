f1e9cdc92fad2242aa9cf44f2db8ca4b76d86e1a
SaiDeepika-DA <muktamsaideepikaa@gmail.com>
2026-07-31 18:39:37 +0530

    1st

--- SQL/Discount VS profit by sub category.sql
+++ SQL/Discount VS profit by sub category.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    sub_category,
+    ROUND(AVG(discount) * 100, 2) AS avg_discount_pct,
+    ROUND(SUM(profit), 2)          AS total_profit
+FROM orders
+GROUP BY sub_category
+ORDER BY avg_discount_pct DESC;
