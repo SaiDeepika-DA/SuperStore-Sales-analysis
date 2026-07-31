6900cf6eb7c67be01d26a3703f09e7fe9132e6bb
SaiDeepika-DA <muktamsaideepikaa@gmail.com>
2026-07-31 18:37:14 +0530

    1st

--- SQL/profit&margin.sql
+++ SQL/profit&margin.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    category,
+    sub_category,
+    ROUND(SUM(sales), 2)  AS total_sales,
+    ROUND(SUM(profit), 2) AS total_profit,
+    ROUND(SUM(profit) / SUM(sales) * 100, 2) AS profit_margin_pct
+FROM orders
+GROUP BY category, sub_category
+ORDER BY total_profit ASC;
